6ac954e97b56f29a684ca2211eadcd21ef0542e1
PolinaRoo <polina.roo10@gmail.com>
2023-07-26 18:56:50 +0300

    Classwork #1

---
+++ Login/Login/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Polina Tereshchenko on 24.07.2023.
+//
+
+import Foundation
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
