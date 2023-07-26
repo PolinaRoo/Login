0188cd0512acaa977554195f70595b5140965dcd
PolinaRoo <polina.roo10@gmail.com>
2023-07-26 20:06:50 +0300

    Homework #1

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -8,14 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var envelopImageView: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var lockImageView: UIImageView!
+    @IBOutlet weak var emailLine: UIView!
+    @IBOutlet weak var passwordLine: UIView!
+    @IBOutlet weak var dontHaveAnAccountLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
     
     //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("viewDidLoad")
+        configureLoginButton()
     }
-
-
+    
+    //MARK: - Actions
+    @IBAction func loginButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButtonAction(_ sender: Any) {
+    }
+    
+    
+    // MARK: Methods
+    private func configureLoginButton() {
+        loginButton.layer.shadowColor = (UIColor(named: "orange") ?? UIColor.darkGray).cgColor
+        loginButton.layer.shadowOffset = CGSize(width: 4, height: 6)
+        loginButton.layer.shadowOpacity = 0.5
+        loginButton.layer.shadowRadius = 5
+    }
 }
 
